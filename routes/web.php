0e1cfae686ab569e43351443b1498429d30f2570
novilfahlevy <novilfreon@gmail.com>
2025-04-28 10:24:31 +0800

    pendaftaran

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,15 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegistrationApplicationController;
 use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/pendaftaran', [HomeController::class, 'registration'])->name('registration');
+Route::post('/pendaftaran', [RegistrationApplicationController::class, 'store'])->name('registration.store');
+Route::get('/pendaftaran-berhasil', [RegistrationApplicationController::class, 'showSuccess'])->name('registration.success');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
