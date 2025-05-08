867bf20bd11bade7ef355ecc5270b8cb2ff36830
novilfahlevy <novilfreon@gmail.com>
2025-05-08 20:54:59 +0800

    fix header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,12 +45,12 @@
             <div class="container d-flex justify-content-center justify-content-md-between">
                 <div class="contact-info d-flex align-items-center">
                     <i class="bi bi-envelope d-flex align-items-center"><a
-                            href="mailto:contact@example.com">contact@example.com</a></i>
-                    <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
+                            href="mailto:smaimmanuelkalasan@gmail.com">smaimmanuelkalasan@gmail.com</a></i>
+                    <i class="bi bi-phone d-flex align-items-center ms-4"><span>0274 4469287</span></i>
                 </div>
                 <div class="social-links d-none d-md-flex align-items-center">
-                    <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                    <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                    <a href="https://smaimmanuelkalasan.sch.id" target="_blank" class="website"><i class="bi bi-globe"></i></a>
+                    <a href="https://www.instagram.com/smaimmanuelkalasan" target="_blank" class="instagram"><i class="bi bi-instagram"></i></a>
                 </div>
             </div>
         </div><!-- End Top Bar -->
