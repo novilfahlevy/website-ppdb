28fe61f229c6ec21df7e56e97a1468f753d7a70c
novilfahlevy <novilfreon@gmail.com>
2025-05-08 14:55:30 +0800

    fix responsive table

--- resources/views/admin/archived-registration/index.blade.php
+++ resources/views/admin/archived-registration/index.blade.php
@@ -1,25 +1,23 @@
 @extends('admin.layouts.app')
 
 @section('content')
-    <a href="{{ route('registrations.create') }}" class="btn btn-primary mb-3">Tambah Pendaftaran</a>
-
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Data Pendaftaran</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Data Arsip Pendaftaran</h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table table-striped table-hover table-bordered" width="100%" cellspacing="0">
+                <table class="table table-bordered">
                     <thead>
                         <tr>
-                            <th>NO</th>
-                            <th>Nama</th>
-                            <th>Tahun Ajaran</th>
-                            <th>Mulai Dari</th>
-                            <th>Sampai Dengan</th>
-                            <th>Status</th>
-                            <th>Tanggal Dibuat</th>
-                            <th>Aksi</th>
+                            <th scope="col">NO</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Tahun Ajaran</th>
+                            <th scope="col">Mulai Dari</th>
+                            <th scope="col">Sampai Dengan</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Tanggal Dibuat</th>
+                            <th scope="col">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -30,20 +28,26 @@
                                 <td>{{ $registration->academic_year ?: '-' }}</td>
                                 <td>{{ $registration->start_date->translatedFormat('l, d F Y') }}</td>
                                 <td>{{ $registration->end_date->translatedFormat('l, d F Y') }}</td>
-                                <td>{{ $registration->is_open ? 'Dibuka' : 'Selesai' }}</td>
-                                <td>{{ $registration->created_at->translatedFormat('l, d F Y H:i:s') }}</td>
                                 <td>
+                                    @if ($registration->is_open)
+                                        <span class="badge bg-success text-white">Dibuka</span>
+                                    @else
+                                        <span class="badge bg-danger text-white">Ditutup</span>
+                                    @endif
+                                </td>
+                                <td>{{ $registration->created_at->translatedFormat('l, d F Y H:i') }}</td>
+                                <td class="d-inline-flex flex-column">
                                     <a href="{{ route('registrations.edit', ['id' => $registration->id]) }}"
-                                        class="btn btn-sm btn-primary">Edit</a>
+                                        class="btn btn-sm btn-primary mb-2">Edit</a>
                                     <form action="{{ route('registrations.destroy', ['id' => $registration->id]) }}"
                                         method="POST" style="display: inline;">
                                         @csrf
                                         @method('DELETE')
-                                        <button type="submit" class="btn btn-sm btn-danger"
-                                            onclick="return confirm('Apakah anda yakin ingin menghapus pendaftaran ini?')">Delete</button>
+                                        <button type="submit" class="btn btn-sm btn-danger mb-2"
+                                            onclick="return confirm('Apakah anda yakin ingin menghapus pendaftaran ini?')">Hapus</button>
                                     </form>
                                     <a href="{{ route('registrations.show', ['id' => $registration->id]) }}"
-                                        class="btn btn-sm btn-info btn-outline">Lihat Pendaftar</a>
+                                        class="btn btn-sm btn-info btn-outline mb-2">Lihat Pendaftar</a>
                                     <form action="{{ route('registrations.unarchive', ['id' => $registration->id]) }}"
                                         method="POST" style="display: inline;">
                                         @csrf
--- resources/views/admin/registration/index.blade.php
+++ resources/views/admin/registration/index.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.app')
 
 @section('content')
-    <a href="{{ route('registrations.create') }}" class="btn btn-primary mb-3">Tambah Pendaftaran</a>
+    <a href="{{ route('registrations.create') }}" class="btn btn-primary mb-3">Buka Pendaftaran</a>
 
     <div class="card shadow mb-4">
         <div class="card-header py-3">
@@ -9,17 +9,17 @@
         </div>
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table table-striped table-hover table-bordered" width="100%" cellspacing="0">
+                <table class="table table-bordered">
                     <thead>
                         <tr>
                             <th>NO</th>
-                            <th>Nama</th>
-                            <th>Tahun Ajaran</th>
-                            <th>Mulai Dari</th>
-                            <th>Sampai Dengan</th>
-                            <th>Status</th>
-                            <th>Tanggal Dibuat</th>
-                            <th>Aksi</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Tahun Ajaran</th>
+                            <th scope="col">Mulai Dari</th>
+                            <th scope="col">Sampai Dengan</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Tanggal Dibuat</th>
+                            <th scope="col">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -37,19 +37,19 @@
                                         <span class="badge bg-danger text-white">Ditutup</span>
                                     @endif
                                 </td>
-                                <td>{{ $registration->created_at->translatedFormat('l, d F Y H:i:s') }}</td>
-                                <td>
+                                <td>{{ $registration->created_at->translatedFormat('l, d F Y H:i') }}</td>
+                                <td class="d-inline-flex flex-column">
                                     <a href="{{ route('registrations.edit', ['id' => $registration->id]) }}"
-                                        class="btn btn-sm btn-primary">Edit</a>
+                                        class="btn btn-sm btn-primary mb-2">Edit</a>
                                     <form action="{{ route('registrations.destroy', ['id' => $registration->id]) }}"
                                         method="POST" style="display: inline;">
                                         @csrf
                                         @method('DELETE')
-                                        <button type="submit" class="btn btn-sm btn-danger"
-                                            onclick="return confirm('Apakah anda yakin ingin menghapus pendaftaran ini?')">Delete</button>
+                                        <button type="submit" class="btn btn-sm btn-danger mb-2"
+                                            onclick="return confirm('Apakah anda yakin ingin menghapus pendaftaran ini?')">Hapus</button>
                                     </form>
                                     <a href="{{ route('registrations.show', ['id' => $registration->id]) }}"
-                                        class="btn btn-sm btn-info btn-outline">Lihat Pendaftar</a>
+                                        class="btn btn-sm btn-info btn-outline mb-2">Lihat Pendaftar</a>
                                     <form action="{{ route('registrations.archive', ['id' => $registration->id]) }}"
                                         method="POST" style="display: inline;">
                                         @csrf
