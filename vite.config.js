64b577e7ff302969ba78e37fc6f674e66ea011fc
novilfahlevy <novilfreon@gmail.com>
2025-05-09 20:07:25 +0800

    fix dropdown

--- vite.config.js
+++ vite.config.js
@@ -12,10 +12,10 @@ export default defineConfig({
                 'resources/js/admin/app.js',
 
                 'resources/css/admin/sb-admin-2.min.css',
-                'resources/js/admin/sb-admin-2.min.js',
+                'resources/js/admin/chart.js',
 
                 'resources/js/admin/jquery.min.js',
-                'resources/js/admin/chart.js',
+                'resources/js/admin/bootstrap.bundle.min.js',
             ],
             refresh: true,
         }),
